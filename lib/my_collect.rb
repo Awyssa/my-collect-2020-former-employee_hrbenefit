804b7af41d8881e4794253e3d2ce244bf88cfc90
Michael Ernest <m.hendersun@gmail.com>
2020-10-05 16:15:12 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,18 @@
 
 
 def my_collect(array)
-  array.collect do |lang|
-  end
+  array.collect { |lang| lang.upcase}
 end
+
+
+
+
+# old attempt
+# def my_collect(array)
+#   new_array = []
+#   array.collect do |lang|
+#     lang.upcase
+#     new_array << lang
+#   end
+#   return new_array
+# end
