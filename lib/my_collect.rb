74bb7731ddc5a8284fb2dfc68d94ce2caff3e41c
Michael Ernest <m.hendersun@gmail.com>
2020-10-05 16:18:34 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,24 @@
 
 
+def my_collect(array)
+  i = 0
+  collect = []
+  while i < array.length
+    collect << yield(array[i])
+    i+=1
+  end
+  collect
+end
+
+
+
+
+# old attempt
+# def my_collect(array)
+#   new_array = []
+#   array.collect do |lang|
+#     lang.upcase
+#     new_array << lang
+#   end
+#   return new_array
+# end
